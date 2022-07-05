8307a8dedb869fb5c0c506ae1dbe5dd2777d3cb8
JuanantonioVYT <78661859+JuanantonioVYT@users.noreply.github.com>
2022-07-06 01:44:56 +0200

    Add files via upload

--- src/LC/Event/Protection.php
+++ src/LC/Event/Protection.php
@@ -13,22 +13,22 @@ class Protection implements \pocketmine\event\Listener {
 	public function onDamage(\pocketmine\event\entity\EntityDamageEvent $ev) {
 		$pl = $ev->getEntity();
 		if ($pl->getWorld() === Server::getInstance ()->getWorldManager()->getDefaultWorld()) {
-			    $ev->cancel(false);
+			    $ev->cancel();
 		}
 	}
 	
 	public function onExplosion(\pocketmine\event\entity\ExplosionPrimeEvent $ev) {
-		$ev->cancel(true);
+		$ev->cancel();
 		
 	}
 	
 	public function onBreak(\pocketmine\event\block\BlockBreakEvent $ev) {
 		$pl = $ev->getPlayer();
 		if ($pl->getWorld() === Server::getInstance ()->getWorldManager()->getDefaultWorld()) {
-			if ($pl->hasPermission("lobbycore.actions")) {
-				$ev->cancel(false);
+			if (!$pl->hasPermission("lobbycore.break")) {
+				$ev->cancel();
 			} else {
-				$ev->cancel(true);
+				$ev->cancel();
 				$pl->sendPopup("§cYou have don't permissions for break blocks");
             }
 		}
@@ -37,10 +37,10 @@ class Protection implements \pocketmine\event\Listener {
 	public function onPlace(\pocketmine\event\block\BlockPlaceEvent $ev) {
 		$pl = $ev->getPlayer();
 		if ($pl->getWorld() === Server::getInstance ()->getWorldManager()->getDefaultWorld()) {
-			if ($pl->hasPermission("lobbycore.actions")) {
-				$ev->cancel(false);
+			if (!$pl->hasPermission("lobbycore.place")) {
+				$ev->cancel();
 			} else {
-				$ev->cancel(true);
+				$ev->cancel();
 				$pl->sendPopup("§cYou have don't permissions for place blocks");
 			}
 		}
@@ -49,14 +49,14 @@ class Protection implements \pocketmine\event\Listener {
 	public function onDrop(\pocketmine\event\player\PlayerDropItemEvent $ev) {
 		$pl = $ev->getPlayer();
 		if ($pl->getWorld() === Server::getInstance ()->getWorldManager()->getDefaultWorld()) {
-			$ev->cancel(true);
+			$ev->cancel();
 		}
 	}
 
 	public function onExhaust(\pocketmine\event\player\PlayerExhaustEvent $ev) {
 		$pl = $ev->getPlayer();
 		if ($pl->getWorld() === Server::getInstance ()->getWorldManager()->getDefaultWorld()) {
-			$ev->cancel(true);
+			$ev->cancel();
 		}
 	}
 }
