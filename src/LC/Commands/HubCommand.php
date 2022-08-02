c8b889ee5fa5869bc1d3012a8feb67aa88f60490
JuanantonioVYT <78661859+JuanantonioVYT@users.noreply.github.com>
2022-08-02 21:32:12 +0200

    New Config hub!

--- src/LC/Commands/HubCommand.php
+++ src/LC/Commands/HubCommand.php
@@ -11,18 +11,24 @@ use pocketmine\plugin\Plugin;
 use pocketmine\item\ItemFactory;
 use pocketmine\item\Item;
 
+use LC\LobbyCore;
+
 class HubCommand extends Command {
   
+  private $plugin;
+
   public function __construct(){
     parent::__construct("hub", "hub command", null, ["spawn"]);
   }
   
   public function execute(CommandSender $player, string $label, array $args){
     if($player instanceof Player){
+       $this->plugin = LobbyCore::getInstance();
        $player->teleport($player->getServer()->getWorldManager()->getDefaultWorld()->getSafeSpawn());
        $player->getInventory()->clearALL();
        $player->getArmorInventory()->clearALL();
-       $player->sendMessage(MG::GREEN . "Volviste al lobby" . MG::YELLOW . $player->getName());
+       $player->sendMessage(str_replace(["{player}"], [$player->getName()], $this->plugin->getConfig()->get("Hub-Message")));
+       $player->sendTitle(str_replace(["{player}"], [$player->getName()], $this->plugin->getConfig()->get("Hub-Title")));
 
        $slot1 = ItemFactory::getInstance()->get(345, 0, 1)->setCustomName("Games");
        $slot2 = ItemFactory::getInstance()->get(54, 0, 1)->setCustomName("Cosmeticos");
