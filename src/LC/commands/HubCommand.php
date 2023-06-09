ca8941609339e9d88969691170f728180bbc97a4
JuanantonioVYT <78661859+JuanantonioVYT@users.noreply.github.com>
2023-06-09 20:42:28 +0200

    Fix permissions, new Permission /hub
    new permission /hub = lobbycore.command.hub

--- src/LC/commands/HubCommand.php
+++ src/LC/commands/HubCommand.php
@@ -29,26 +29,30 @@ class HubCommand extends Command
     public function execute(CommandSender $player, string $label, array $args)
     {
         if ($player instanceof Player) {
-            $this->plugin = LobbyCore::getInstance();
-            $player->teleport($player->getServer()->getWorldManager()->getDefaultWorld()->getSafeSpawn());
-            $player->getInventory()->clearALL();
-            $player->getArmorInventory()->clearALL();
-            $player->sendMessage(str_replace(["{player}"], [$player->getName()], $this->plugin->getConfig()->get("Hub-Message")));
-            $player->sendTitle(str_replace(["{player}"], [$player->getName()], $this->plugin->getConfig()->get("Hub-Title")));
+            if (!$player->hasPermission("lobbycore.command.hub")) {
+                $player->sendMessage("No tienes permisos");
+            } else {
+                $this->plugin = LobbyCore::getInstance();
+                $player->teleport($player->getServer()->getWorldManager()->getDefaultWorld()->getSafeSpawn());
+                $player->getInventory()->clearALL();
+                $player->getArmorInventory()->clearALL();
+                $player->sendMessage(str_replace(["{player}"], [$player->getName()], $this->plugin->getConfig()->get("Hub-Message")));
+                $player->sendTitle(str_replace(["{player}"], [$player->getName()], $this->plugin->getConfig()->get("Hub-Title")));
 
-            $item1 = VanillaItems::COMPASS();
-            $item1->setCustomName("Games");
+                $item1 = VanillaItems::COMPASS();
+                $item1->setCustomName("Games");
 
-            $item2 = VanillaItems::DIAMOND_AXE();
-            $item2->setCustomName("Cosmeticos");
+                $item2 = VanillaItems::DIAMOND_AXE();
+                $item2->setCustomName("Cosmeticos");
 
-            $item3 = VanillaItems::BOOK();
-            $item3->setCustomName("Informacion");
+                $item3 = VanillaItems::BOOK();
+                $item3->setCustomName("Informacion");
 
 
-            $player->getInventory()->setItem(0, $item1);
-            $player->getInventory()->setItem(4, $item2);
-            $player->getInventory()->setItem(8, $item3);
+                $player->getInventory()->setItem(0, $item1);
+                $player->getInventory()->setItem(4, $item2);
+                $player->getInventory()->setItem(8, $item3);
+            }
         }
     }
 }
